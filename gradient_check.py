minor gradient fixes of Linear class + added test for gradietn check of Dense

gradient_check.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from layer import Layer
+from layer import Layer, NnLayer
 
 
 class GradientCheck:
@@ -52,3 +52,24 @@ class GradientCheck:
             return loss, grad
 
         return GradientCheck.check_gradient(helper_func, x, delta, tol)
+
+    @staticmethod
+    def check_layer_param_gradient(layer: NnLayer, X, param_name, delta=1e-5, tol=1e-4):
+        param = layer.params()[param_name]
+        initial_w = param.data
+
+        output = layer.forward(X)
+        output_w = np.random.randn(*output.shape)
+
+        def helper(w):
+            param.data = w
+            output = layer.forward(X)
+            loss = np.sum(output * output_w)
+            d_out = np.ones_like(output) * output_w
+            layer.backward(d_out)
+            grad = param.grad
+
+            return loss, grad
+
+        return GradientCheck.check_gradient(helper, initial_w, delta, tol)
+

layer.py
@@ -26,8 +26,8 @@ class NnLayer(Layer):
 
 class Linear(NnLayer):
     def __init__(self, n_input, n_output):
-        self.W = Value(n_input, n_output)
-        self.B = Value(1, n_output)
+        self.W = Value(0.001 * np.random.randn(n_input, n_output))
+        self.B = Value(0.001 * np.random.randn(1, n_output))
 
         self.X = None
 
@@ -38,9 +38,9 @@ class Linear(NnLayer):
 
     def backward(self, d_out):
         self.W.grad = np.dot(self.X.T, d_out)
-        self.B.grad = np.sum(d_out, axis=1)
+        self.B.grad = np.reshape(np.sum(d_out, axis=0), (1, -1))
 
-        d_pred = np.dot(self.W.data, d_out.T)
+        d_pred = np.dot(d_out, self.W.data.T)
 
         return d_pred
 

main.py
@@ -1,11 +1,31 @@
 import numpy as np
 
 from gradient_check import GradientCheck
-from layer import ReLU
+from layer import ReLU, Linear
 
-if __name__ == "__main__":
+
+def test_relu():
     X = np.array([[1, -2, 3],
                   [-1, 2, 0.1]
                   ])
 
     assert GradientCheck.check_layer_gradient(ReLU(), X)
+
+
+def test_dense():
+    X = np.array([[1, -2, 3],
+                  [-1, 2, 0.1]
+                  ])
+
+    assert GradientCheck.check_layer_gradient(Linear(3, 4), X)
+    assert GradientCheck.check_layer_param_gradient(Linear(3, 4), X, 'W')
+    assert GradientCheck.check_layer_param_gradient(Linear(3, 4), X, 'B')
+
+
+
+if __name__ == "__main__":
+    test_dense()
+
+
+
+

value.py
@@ -3,6 +3,6 @@ import random
 
 
 class Value:
-    def __init__(self, in_num, out_num):
-        self.data = np.ones(shape=(in_num, out_num)) * (1 / np.sqrt(in_num) * random.uniform(in_num, out_num))  # Xavier
-        self.grad = np.zeros(shape=(in_num, out_num))
+    def __init__(self, value):
+        self.data = value
+        self.grad = np.zeros_like(value)