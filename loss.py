added l2 reg, gradient checks, fixed softmax computing

loss.py
@@ -1,18 +1,23 @@
 import numpy as np
 
 
-def _compute_softmax(y_pred):
-    y_pred_copy = y_pred - np.max(y_pred, axis=1)
-
-    return np.exp(y_pred_copy) / np.sum(np.exp(y_pred))
+def compute_softmax(y_pred):
+    exps = np.exp(y_pred - np.max(y_pred, axis=1, keepdims=True))
+    return exps / np.sum(exps, axis=1, keepdims=True)
 
 
 class CrossEntropyLoss:
 
     def __call__(self, y_pred, y_true):
-        softmax = _compute_softmax(y_pred)
+        batch_size = y_pred.shape[0]
+        num_classes = y_pred.shape[1]
 
-        loss = -np.sum(y_true * np.log(softmax), axis=1)
-        grad = y_pred - softmax
+        y_true_one_hot = np.zeros((batch_size, num_classes))
+        y_true_one_hot[np.arange(batch_size), y_true] = 1
+
+        logits = compute_softmax(y_pred)
+
+        loss = -np.sum(y_true_one_hot * np.log(logits))
+        grad = logits - y_true_one_hot
 
         return loss, grad

value.py
@@ -1,8 +1,10 @@
 import numpy as np
-import random
 
 
 class Value:
     def __init__(self, value):
         self.data = value
         self.grad = np.zeros_like(value)
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"