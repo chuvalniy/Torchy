tests for CS loss and batchnorm + major fixes gradient logic for CS loss

loss.py
@@ -12,9 +12,7 @@ class CrossEntropyLoss:
         batch_size = y_pred.shape[0]
         num_classes = y_pred.shape[1]
 
-        y_true_one_hot = np.zeros((batch_size, num_classes))
-        y_true_one_hot[np.arange(batch_size), y_true] = 1
-
+        y_true_one_hot = np.identity(num_classes)[y_true.reshape(-1)]
         logits = compute_softmax(y_pred)
 
         loss = -np.sum(y_true_one_hot * np.log(logits))

main.py
@@ -3,15 +3,14 @@ import numpy as np
 from sklearn.datasets import make_classification
 
 import layer
-import optim
 import sequential
-from loss import CrossEntropyLoss
-from scheduler import ReduceLROnPlateau
+from tests.gradient_check import GradientCheck
 from tests.test_layer import test_batchnorm1d
+from tests.test_loss import test_cross_entropy_single, test_softmax, test_cross_entropy_batch
 
 
 def train_model():
-    X, y = make_classification(n_samples=50, n_features=5, n_redundant=0)
+    X, y = make_classification(n_samples=10, n_features=5, n_redundant=0)
 
     model = sequential.Sequential(
         layer.Linear(n_input=5, n_output=8),
@@ -20,27 +19,26 @@ def train_model():
         layer.Linear(n_input=8, n_output=2)
     )
 
-    criterion = CrossEntropyLoss()
-    optimizer = optim.MomentumSGD(model.params(), lr=1e-1, weight_decay=1e-5)
-    scheduler = ReduceLROnPlateau(optimizer)
-
-    for epoch in range(10):
-        predictions = model(X)
-        loss, grad = criterion(predictions, y)
-
-        optimizer.zero_grad()
-        model.backward(grad)
-        optimizer.step()
-
-        indices = np.argmax(predictions, axis=1)
-        accuracy = (np.sum(indices == y)) / y.shape[0]
-
-        print(f"Epoch: {epoch}, Loss: {loss}, Accuracy: {accuracy}, Learning rate: {optimizer.lr}")
-
-        scheduler.step(loss)
+    GradientCheck.check_model_gradient(model, X, y)
+    # criterion = CrossEntropyLoss()
+    # optimizer = optim.MomentumSGD(model.params(), lr=1e-1, weight_decay=1e-5)
+    # scheduler = ReduceLROnPlateau(optimizer)
+    #
+    # for epoch in range(100):
+    #     predictions = model(X)
+    #     loss, grad = criterion(predictions, y)
+    #
+    #     optimizer.zero_grad()
+    #     model.backward(grad)
+    #     optimizer.step()
+    #
+    #     indices = np.argmax(predictions, axis=1)
+    #     accuracy = (np.sum(indices == y)) / y.shape[0]
+    #
+    #     print(f"Epoch: {epoch}, Loss: {loss}, Accuracy: {accuracy}, Learning rate: {optimizer.lr}")
+    #
+    #     scheduler.step(loss)
 
 
 if __name__ == "__main__":
     np.random.seed(42)
-
-    train_model()

tests/test_layer.py
@@ -1,8 +1,9 @@
 import numpy as np
+from sklearn.datasets import make_classification
 
 from layer import Linear, ReLU, BatchNorm1d
 from tests.gradient_check import GradientCheck
-from sklearn.datasets import make_classification
+
 
 def test_linear():
     X = np.array([[1, -2, 3],