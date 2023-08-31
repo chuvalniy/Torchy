14c94dee36e3ebde69a132c39fac2c94c2d496e2
Chuvalniy <chuvalik@gmail.com>
2023-08-31 11:16:59 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.3.2'
-DESCRIPTION = 'NumPy based neural network package'
+VERSION = '0.2.3.3'
+DESCRIPTION = 'NumPy based neural network package with PyTorch-like API'
+
+with open("README.md", "r") as fn:
+    long_description = fn.read()
 
 setup(
     name="torchy-nn",
@@ -9,6 +12,7 @@ setup(
     author="Valentin Belyaev",
     author_email="chuvalik.work@gmail.com",
     description=DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     url="https://github.com/chuvalniy/Torchy",
     install_requires=['numpy', 'scikit-learn', 'pytest'],
